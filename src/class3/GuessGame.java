43ad6d1968fb92135f60fa599368d82ffc09b363
wjinshui <ymkscom@gmail.com>
2021-03-12 17:26:09 +0800

    tt

--- src/class3/GuessGame.java
+++ src/class3/GuessGame.java
@@ -1,6 +1,7 @@
 package class3;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class GuessGame
 {
@@ -19,14 +20,41 @@ public class GuessGame
 				{
 					flags[number] = true;
 					answers[i] = number;
+					
 					break;
 				}
 			}
+			
 		}
-		
-		for (int i : answers)
+		Scanner scanner = new Scanner(System.in);
+		while(true)
 		{
-			System.out.println(i);
+			System.out.println("请输入四个0-9之间的数字: ");
+			int[] inputs = new int[answers.length];
+			for (int i = 0; i < inputs.length; i++)
+			{
+				inputs[i] = scanner.nextInt();
+			}
+			int a =0, b=0;
+			for (int i = 0; i < answers.length; i++)
+			{
+				for (int j = 0; j < inputs.length; j++)
+				{
+					if(inputs[j] == answers[i])
+						b ++;
+				}
+			}
+			for (int i = 0; i < inputs.length; i++)
+			{
+				if(answers[i] == inputs[i])
+					a++;
+			}
+			b = b - a;
+			System.out.println(a+ "A" + b + "B");
+			if(a == 4)
+			{
+				System.out.println("好厉害啊。。游戏结束");
+			}
 		}
 
 	}
